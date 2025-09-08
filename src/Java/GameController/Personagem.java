1e873664635fc51369abd1bce95f5e4e3ff206b2
Arthur Ricartte <a.ricartte@gmail.com>
2025-09-08 17:04:37 -0300

    Criando super classe Personagem e uma sub classe(Sakura) com exemplo.

--- src/Java/GameController/Personagem.java
+++ src/Java/GameController/Personagem.java
@@ -1,17 +1,45 @@
 package GameController;
 
+//Super Classe
 public class Personagem {
 
-    //Objeto Personagem (Menina) que possui Nome e Pontuação
+    //Objeto Personagem (Menina) que possui Nome, Pontuação e idade.
     String nome;
+    int idade;
     int pontuacao;
 
     //Construtor
-    public Personagem(String nome, int pontuacaoInicial){
-
+    public Personagem(String nome, int idade, int pontuacaoInicial){
         this.nome = nome;
+        this.idade = idade;
         this.pontuacao = pontuacaoInicial;
 
     }
+
+    //Encapsulamento:
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setPontuacao(int pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
 
 }
